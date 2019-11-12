d3512afc7d3d9b6c1ef635bb58e43217eaee8197
Danny <dmcadam2000@gmail.com>
2019-11-12 23:19:34 +0000

    Initial Commit

---
+++ StackImplementation/IntStack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace week3StacksandQueues
+{
+    class IntStack
+
+    {
+
+        private const int maxsize = 10;
+        private int top = 0;
+        private int[] array = new int[maxsize];
+
+        public void Push(int value)
+
+        {
+            array[top++] = value;
+        }
+
+
+
+        public int Pop()
+
+        {
+            return array[--top];
+        }
+
+
+
+        public int Peek()
+
+        {
+            //Top is location where next value will be added to queue, so Top -1 shows current 
+            return array[(top - 1)];
+        }
+
+        public bool IsEmpty()
+
+        {
+            
+            return top <= 0;// insert code here
+        }
+
+
+
+        public bool IsFull()
+
+        {
+            return top == maxsize;
+        }
+
+
+
+    }
+}
---
+++ StackImplementation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace week3StacksandQueues
+{
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+            IntStack mystack = new IntStack();
+            mystack.Push(10);
+            System.Console.WriteLine(mystack.Peek() + " Peek");
+            System.Console.WriteLine(mystack.Pop());
+
+
+            mystack.Push(20);
+            mystack.Push(30);
+            mystack.Push(40);
+
+            System.Console.WriteLine(mystack.Pop());
+            System.Console.WriteLine(mystack.IsEmpty());
+            System.Console.WriteLine(mystack.Pop());
+            System.Console.WriteLine(mystack.IsEmpty());
+            System.Console.WriteLine(mystack.Pop());
+            System.Console.WriteLine(mystack.IsEmpty());
+
+            Random rand = new Random();
+            while(!mystack.IsFull())
+            {
+                int i = rand.Next(100);
+                mystack.Push(i);
+            }
+
+            while(!mystack.IsEmpty())
+            {
+                System.Console.WriteLine(mystack.Pop());
+            }
+
+            System.Console.ReadKey();
+
+        }
+
+    }
+}
